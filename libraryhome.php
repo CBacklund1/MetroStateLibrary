783dfeed85722bb1cc8d6dba8c4fd83f67436de3
CBacklund1 <89429072+CBacklund1@users.noreply.github.com>
2022-10-04 09:52:44 -0500

    work in progress home page

---
+++ libraryhome.php
@@ -0,0 +1,55 @@
+<!-- ICS 499-50 Fall 2022
+MetroState Library 
+Group 4 Authors: Crystal Backlund, Arun Markandu, 
+Pao Teng Thao, Xiong Yang -->
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles.css" />
+    <title>MetroState Library Home Page</title>
+  </head>
+  <body>
+    <!--script for header-->
+    <script language="javascript" type="text/javascript" src="LibraryHeader.txt"></script>
+
+
+    <div class="banner">
+		<h1>Join the MetroState Library! <h1>
+	  </div>
+	  <br></br>
+	  <div class = "mission">
+	  <div class = "mission1">
+		  <h1> Saves Money</h1> 
+		  <h4>Borrow the books you need for FREE</h4>
+		
+	  </div>
+	  <div class = "mission2">
+		  <h1>Saves Time</h1>
+		  <h4>This is your one-stop-shop for books.</h4>
+		
+		</div>
+	  <div class = "mission3">
+		  <h1>Saves Books</h1>
+		  <h4>Recycle books by borrowing and returning them, as often as you'd like.</h4>
+		  
+		</div>
+</div>
+        </body>
+	<footer class = "signin_footer">
+		<h1>Join Today</h1> <br>
+		<h4>Questions or any last reservations? Reach out to us.</h4><br><br>
+		<form method="POST" action="contactform.php">
+			<input type = "submit" value = "Contact Us"/>
+		</form>
+	<!--script for footer-->
+	<script language="javascript" type="text/javascript" 
+  src="LibraryFooter.txt"></script>
+	</body>
+	</html>
