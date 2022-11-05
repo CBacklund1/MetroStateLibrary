d17842ba0954a118de64f28490127efae5974650
Crystal Backlund <nd4521lr@go.minnstate.edu>
2022-11-05 11:15:45 -0500

    fixed css error

--- libraryhome.php
+++ libraryhome.php
@@ -12,7 +12,7 @@ Pao Teng Thao, Xiong Yang -->
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="css/styles.css" />
     <title>MetroState Library Home Page</title>
   </head>
   <body>
