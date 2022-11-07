5e534eec0e2c710d4c83bd7ff07fc05ed2c00d62
Xiong Yang <xionggie@gmail.com>
2022-11-07 11:14:26 -0600

    update checkoutsubmit.php     will display a success or fail checkout message with a table of checkout books.     added wrong commit to checkoutform. I did not make anything changes there

--- checkoutsubmit.php
+++ checkoutsubmit.php
@@ -1,61 +1,53 @@
 <?php
+    // User input Variables
+    // $bookname = $_REQUEST['bookname'];
+    // echo $bookname;
+    // $bookid = $_REQUEST['bookid'];
+    // echo $bookid;
+    // $isbn = $_REQUEST['isbn'];
+    // echo $isbn;
 
-// User input Variables
-$bookname = $_REQUEST['bookname'];
-$bookid = $_REQUEST['bookid'];
+    // setcookie("bookid", $bookid);
+    $selectedBooks = $_REQUEST['selectedBookLists'];
+    setcookie("selectedBooks", $selectedBooks);
 
-$isbn = $_REQUEST['isbn'];
-setcookie("bookid", $bookid);
-
-
-
-$document_root = $_SERVER['DOCUMENT_ROOT'];
+    $checkedoutBooks = $_REQUEST['checkedoutBooks'];
+    setcookie("checkedoutBooks", $checkedoutBooks);
+    $document_root = $_SERVER['DOCUMENT_ROOT'];
 ?>
   
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script language="javascript" type="text/javascript" src="js/lib.js" ></script>
-    <link rel="stylesheet" href="css/styles.css" />
-    <!--Insert Page Name-->
-        <title>Contact Us</title>
-</head>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script language="javascript" type="text/javascript" src="js/lib.js" ></script>
+        <link rel="stylesheet" href="css/styles.css" />
+        <!--script for header-->
+        <script language="javascript" type="text/javascript" src="LibraryHeader.txt"></script>
+        <!--Insert Page Name-->
+        <title>Checkout Successfully</title>
+    </head>
 
-<body onload = "checkoutBook()">
-    <!--script for header-->
-    <script language="javascript" type="text/javascript" src="LibraryHeader.txt"></script>
-    <!--Insert Page Heading-->
-    
-	<div class="heading">
-    <br/><br/>
-        <h1>MetroState Library</h1>
-    </div>
-    <br></br>
-    <!--Insert Body of Page-->
-    <div class="body" >
+    <body onload = "checkoutBook()">
+        <!--Insert Page Heading-->
+        <div class="heading">
+            <br/><br/>
+            <h1>MetroState Library</h1>
+        </div>
         
-       <?php
+        <br></br>
 
-echo 'You have checked out book:' . $_POST['isbn'] .' - id: ' . $_POST['isbn'] .'<br />';
-echo 'Please go to the counter to pickup your book.';
+        <!--Insert Body of Page-->
+        <div class="body">
+            <p id="checkoutmsg"></p>
+        </div>
+    </body>
 
-
-?> 
-
-<p id="checkoutmsg"></p>
-	
-	
-	
-	
-    </div>
-</body>
-<br></br>
-<!--script for footer-->
-<script language="javascript" type="text/javascript" src="LibraryFooter.txt"></script>
-<!--End Page-->
-
+    <br><br><br>
+    <!--script for footer-->
+    <script language="javascript" type="text/javascript" src="LibraryFooter.txt"></script>
+    <!--End Page-->
 </html>
