dc0fa53b847e039857f54755ba8b23454bab18bd
ArunMarkandu1 <arun2000markandu@gmail.com>
2022-12-03 18:02:24 -0500

    added input validation for profile form

--- profilepage.php
+++ profilepage.php
@@ -17,11 +17,16 @@ Pao Teng Thao, Xiong Yang -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/styles.css" />
+    <script language="javascript" type="text/javascript" src="js/lib.js" ></script>
+    <style>
+      .error {color: #FF0000;}
+    </style>
     <!--Insert Page Name-->
     <title>Account Page</title>
 </head>
     <!--script for header-->
     <script language="javascript" type="text/javascript" src="LibraryHeader.txt"></script>
+    
     <br></br>
   <body onload = 'validateUser()'>
   <div class="heading">
@@ -60,22 +65,35 @@ Pao Teng Thao, Xiong Yang -->
 <h2>Enter Credit Card Details</h2><br />
           <h4></h4><br />
           <label for="user_id">Enter Your User ID</label><br />
-          <input type="text" id="user_id" name="user_id" placeholder ="User ID" /><br />
+          <input type="text" id="user_id" name="user_id" placeholder ="User ID" oninput='validateUserIdInput()'/>
+          <span class="error" id="user_id_name_error">*User ID is required </span>
+          <br />
 
           <label for="cardholder_name">Enter Cardholders Name:</label><br />
-          <input type="text" id="cardholder_name" name="cardholder_name" placeholder ="Cardholder Name" /><br />
+          <input type="text" id="cardholder_name" name="cardholder_name" placeholder ="Cardholder Name" oninput='validateCardHolderNameInput()'/>
+          <span class="error" id="cardholder_name_error">*Cardholder name is required </span>
+          <br />
 
           <label for="card_number">Enter Card Number</label><br />
-          <input type="text" id="card_number" name="card_number" placeholder = "Card Number"/><br />
+          <input type="text" id="card_number" name="card_number" placeholder = "Card Number" oninput='validateCardNumberInput()'/>
+          <span class="error" id="card_number_error">*Card number is required </span>
+          <br />
 
           <label for="exp_date">Expiration Date</label><br />
-          <input type="text" id="exp_date" name="exp_date" placeholder ="MMDD" /><br />
+          <input type="text" id="exp_date" name="exp_date" placeholder ="MMDD" oninput='validateExpDate()'/>
+          <span class="error" id="exp_date_error">*Expiration date is required </span>
+          <br />
 
           <label for="card_code">Enter 3 or 4 digit Card Code</label><br />
-          <input type="text" id="card_code" name="card_code" placeholder ="000" /><br />
+          <input type="text" id="card_code" name="card_code" placeholder ="000" oninput='validateCardCode()'/>
+          <span class="error" id="card_code_error">*Card code is required </span>
+          <br />
 
           <label for="zip_code">Cardholder's Zip Code</label><br />
-          <input type="text" id="zip_code" name="zip_code" placeholder ="12345" /><br />
+          <input type="text" id="zip_code" name="zip_code" placeholder ="12345" oninput='validateZipCode()'/>
+          <span class="error" id="zip_code_error">*zip code is required </span>
+          <br />
+
           <input type="submit" value="Pay My Fees!!" />
 
 
